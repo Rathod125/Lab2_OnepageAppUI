a3ed59b49be19f94cfa6d4bc16aaeaa84d33f2a7
Rathod125 <tusharrathod5801@gmail.com>
2023-09-19 22:32:49 -0400

    First Commit

--- Lab2_OnepageAppUI/ViewController.swift
+++ Lab2_OnepageAppUI/ViewController.swift
@@ -8,7 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var counter = 0
+    var step = 1
 
+    @IBOutlet weak var numberOutput: UILabel!
+    
+    
+    
+    
+    
+    @IBAction func increment(_ sender: UIButton) {
+        counter = counter + step
+        numberOutput.text = String(counter)
+    }
+    
+    
+    
+    @IBAction func Decrement(_ sender: UIButton) {
+        counter = counter - step
+        numberOutput.text = String(counter)
+    }
+    
+    
+    
+    
+    @IBAction func reset(_ sender: UIButton) {
+        counter = 0
+        step = 1
+        numberOutput.text = String("0")
+    }
+    
+    
+    
+    
+    @IBAction func step2(_ sender: UIButton) {
+        step = 2
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
